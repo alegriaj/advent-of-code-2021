c04335e9a1e1ab01a0248dfb1e5462fc2b69e7f5
Javier Alegria <javier.c.alegria@gmail.com>
2021-12-02 21:30:36 -0800

    Fix incorrect comments

--- day02/aoc2021_day2.js
+++ day02/aoc2021_day2.js
@@ -5,7 +5,7 @@ const { getArrayStringsFromFile } = require('./fileIO');
 const { getFinalPosition } = require('./getFinalPosition');
 const { getFinalPositionWithAim } = require('./getFinalPositionWithAim');
 
-// Import AoC Day2 Input as array of depths
+// Import AoC Day2 Input
 const instructions = getArrayStringsFromFile('./aoc2021_day2_input.txt');
 
 // Question 1 Solution
@@ -15,7 +15,7 @@ console.log(`Horizontal ${final1.horizontal}`); // 1911
 console.log(`Vertical ${final1.vertical}`); // 779
 console.log(`Multiplied ${final1.horizontal * final1.vertical}`); // 1488669
 
-// Question 1 Solution
+// Question 2 Solution
 const final2 = getFinalPositionWithAim(instructions);
 console.log(`Question 2:`);
 console.log(`Horizontal ${final2.horizontal}`); // 1911

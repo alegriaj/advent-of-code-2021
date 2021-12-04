2b2741534e9f59d8516d057e79b1d2134d35ee62
Javier Alegria <javier.c.alegria@gmail.com>
2021-12-04 12:43:01 -0800

    Solution (brute force) to Day04 Question1

---
+++ day04/aoc2021_day4.js
@@ -0,0 +1,9 @@
+// Advent Of Code
+// Day 4 - Giant Squid
+
+const { getWinningScore } = require('./bingo');
+
+// Import AoC Input
+const filename = './aoc2021_day4_input.txt';
+
+console.log(getWinningScore(filename));
---
+++ day04/bingo.test.js
@@ -0,0 +1,9 @@
+const { getWinningScore } = require('./bingo');
+
+test('sample data => 4512', () => {
+  expect(getWinningScore('./sample_data.txt')).toBe(4512);
+});
+
+test('real data => 58838', () => {
+  expect(getWinningScore('./aoc2021_day4_input.txt')).toBe(58838);
+});
